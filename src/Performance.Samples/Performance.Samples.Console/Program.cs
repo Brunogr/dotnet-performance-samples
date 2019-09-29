fdea94916181a62a65d6a419db63b801f3564121
Bruno Gouvêa Roldão <Brunogrbh@hotmail.com>
2019-09-28 22:21:19 -0300

    initial commit

---
+++ src/Performance.Samples/Performance.Samples.Console/Program.cs
@@ -0,0 +1,155 @@
+﻿using Performance.Samples.Data;
+using System;
+using System.Diagnostics;
+
+namespace Performance.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //OnlyNumbers();
+
+            StringGen2();
+
+            Console.ReadKey();
+        }
+
+        private static void StringGen2()
+        {
+            Console.WriteLine("------- GET 1000x -------");
+
+            var data = new SamplesData();
+
+
+            var sw = new Stopwatch();
+            var before2 = GC.CollectionCount(2);
+            var before1 = GC.CollectionCount(1);
+            var before0 = GC.CollectionCount(0);
+
+            sw.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                var values = data.GetAllAsync().GetAwaiter().GetResult();
+            }
+
+            sw.Stop();
+
+
+            var gen0 = GC.CollectionCount(0) - before0;
+            var gen1 = GC.CollectionCount(1) - before1;
+            var gen2 = GC.CollectionCount(2) - before2;
+
+            Console.WriteLine($"Tempo total: {sw.ElapsedMilliseconds}ms");
+            Console.WriteLine($"GC Gen #2  : {gen2}");
+            Console.WriteLine($"GC Gen #1  : {gen1}");
+            Console.WriteLine($"GC Gen #0  : {gen0}");
+            Console.WriteLine("Done!");
+
+        }
+
+        private static void OnlyNumbersTest()
+        {
+
+            Console.WriteLine("------- Regex -------");
+            OnlyNumbersRegex();
+
+            GC.Collect();
+
+            Console.WriteLine("------- Algorithm -------");
+            OnlyNumbersSafe();
+
+            GC.Collect();
+
+            Console.WriteLine("------- Unsafe -------");
+            OnlyNumbersUnsafe();
+
+            GC.Collect();
+        }
+
+
+        private static void OnlyNumbersSafe()
+        {
+            string onlyNumbers = "(234)224@2342354!bcd3445";
+
+            var sw = new Stopwatch();
+            var before2 = GC.CollectionCount(2);
+            var before1 = GC.CollectionCount(1);
+            var before0 = GC.CollectionCount(0);
+
+            sw.Start();
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                var algorithm = OnlyNumbers.Safe(onlyNumbers);
+            }
+            sw.Stop();
+
+
+            var gen0 = GC.CollectionCount(0) - before0;
+            var gen1 = GC.CollectionCount(1) - before1;
+            var gen2 = GC.CollectionCount(2) - before2;
+
+            Console.WriteLine($"Tempo total: {sw.ElapsedMilliseconds}ms");
+            Console.WriteLine($"GC Gen #2  : {gen2}");
+            Console.WriteLine($"GC Gen #1  : {gen1}");
+            Console.WriteLine($"GC Gen #0  : {gen0}");
+            Console.WriteLine("Done!");
+        }
+
+        private static void OnlyNumbersUnsafe()
+        {
+            string onlyNumbers = "(234)224@2342354!bcd3445";
+
+            var sw = new Stopwatch();
+            var before2 = GC.CollectionCount(2);
+            var before1 = GC.CollectionCount(1);
+            var before0 = GC.CollectionCount(0);
+
+            sw.Start();
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                var @unsafe = OnlyNumbers.Unsafe(onlyNumbers);
+            }
+            sw.Stop();
+
+
+            var gen0 = GC.CollectionCount(0) - before0;
+            var gen1 = GC.CollectionCount(1) - before1;
+            var gen2 = GC.CollectionCount(2) - before2;
+
+            Console.WriteLine($"Tempo total: {sw.ElapsedMilliseconds}ms");
+            Console.WriteLine($"GC Gen #2  : {gen2}");
+            Console.WriteLine($"GC Gen #1  : {gen1}");
+            Console.WriteLine($"GC Gen #0  : {gen0}");
+            Console.WriteLine("Done!");
+        }
+
+        private static void OnlyNumbersRegex()
+        {
+            string onlyNumbers = "(234)224@2342354!bcd3445";
+
+            var sw = new Stopwatch();
+            var before2 = GC.CollectionCount(2);
+            var before1 = GC.CollectionCount(1);
+            var before0 = GC.CollectionCount(0);
+
+            sw.Start();
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                var regex = OnlyNumbers.Regex(onlyNumbers);
+            }
+            sw.Stop();
+
+
+            var gen0 = GC.CollectionCount(0) - before0;
+            var gen1 = GC.CollectionCount(1) - before1;
+            var gen2 = GC.CollectionCount(2) - before2;
+
+            Console.WriteLine($"Tempo total: {sw.ElapsedMilliseconds}ms");
+            Console.WriteLine($"GC Gen #2  : {gen2}");
+            Console.WriteLine($"GC Gen #1  : {gen1}");
+            Console.WriteLine($"GC Gen #0  : {gen0}");
+            Console.WriteLine("Done!");
+        }
+    }
+}
---
+++ src/Performance.Samples/Performance.Samples.Data/SamplesData.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Performance.Samples.Data
+{
+    public class SamplesData
+    {
+        private readonly MongoClient client;
+        private readonly IMongoDatabase database;
+        private readonly IMongoCollection<Samples> collection;
+
+        public SamplesData()
+        {
+            client = new MongoClient("mongodb+srv://admin:admin@cluster0-f7uk1.mongodb.net/test?retryWrites=true");
+            database = client.GetDatabase("PerformanceSample");
+            collection = database.GetCollection<Samples>("Samples");
+        }
+
+
+        public async Task<Samples> InsertAsync(Samples entity)
+        {
+            await collection.InsertOneAsync(entity);
+
+            return entity;
+        }
+
+        public async Task<List<Samples>> GetAllAsync()
+        {
+            return (await collection.FindAsync(new BsonDocument())).ToList();
+        }
+
+    }
+
+    public class Samples
+    {
+        public ObjectId Id { get; set; }
+        public string html { get; set; }
+    }
+}
